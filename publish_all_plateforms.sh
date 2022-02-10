7d17e75787714c529bc0be131e9716d7106868c1
sliiri <sliiri@outlook.com>
2022-02-10 21:12:37 +0100

    Zip dotnet publish result to manage releases.

--- publish_all_plateforms.sh
+++ publish_all_plateforms.sh
@@ -3,8 +3,19 @@
 #
 
 dotnet publish --configuration Release -r linux-x64
+zip bb_exporter_linux-x64.zip bb_exporter/bin/Release/net6.0/linux-x64/publish/*.*
+
 dotnet publish --configuration Release -r linux-arm
+zip bb_exporter_linux-arm.zip bb_exporter/bin/Release/net6.0/linux-arm/publish/*.*
+
 dotnet publish --configuration Release -r linux-arm64
+zip bb_exporter_linux-arm64.zip bb_exporter/bin/Release/net6.0/linux-arm64/publish/*.*
+
 dotnet publish --configuration Release -r win-x64
+zip bb_exporter_win-x64.zip bb_exporter/bin/Release/net6.0/win-x64/publish/*.*
+
 dotnet publish --configuration Release -r win-x86
+zip bb_exporter_win-x86.zip bb_exporter/bin/Release/net6.0/win-x86/publish/*.*
+
 dotnet publish --configuration Release -r osx-x64
+zip bb_exporter_osx-x64.zip bb_exporter/bin/Release/net6.0/osx-x64/publish/*.*
